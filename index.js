78e0ce19577d1fc041b6abf52e1e2aeabb338bb5
Brad Ledbetter <bledbett@acninc.com>
2015-03-31 10:48:56 -0400

    Adding check for win32 to use _netrc instead of .netrc file. Adding fallback to USERPROFILE environment variable, too.

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ var fs = require("fs")
  * @api public
  */
 module.exports = exports = function(file) {
-  var home = process.env.HOME || process.env.HOMEPATH;
+  var home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
+    , platform = require('os').platform();
   
   if(!file && !home) return {};
-  file = file || join(home, ".netrc");
+  file = file || join(home, (platform == "win32"?"_netrc":".netrc"));
 
   if(!file || !fs.existsSync(file)) return {};
   var netrc = fs.readFileSync(file, "UTF-8");
