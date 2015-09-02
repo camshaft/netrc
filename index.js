2c12249c7812c33a08a278eb1d5a565595f23d70
camshaft <bytheway.cameron@gmail.com>
2015-09-02 10:37:57 -0600

    cleanup

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /**
  * Module dependencies
  */
-var fs = require("fs")
-  , join = require("path").join;
+
+var fs = require('fs');
+var join = require('path').join;
 
 /**
  * Read and parse .netrc
@@ -11,14 +12,15 @@ var fs = require("fs")
  * @return {Object}
  * @api public
  */
+
 module.exports = exports = function(file) {
   var home = getHomePath();
-  
-  if(!file && !home) return {};
-  file = file || join(home, ".netrc");
 
-  if(!file || !fs.existsSync(file)) return {};
-  var netrc = fs.readFileSync(file, "UTF-8");
+  if (!file && !home) return {};
+  file = file || join(home, '.netrc');
+
+  if (!file || !fs.existsSync(file)) return {};
+  var netrc = fs.readFileSync(file, 'UTF-8');
   return exports.parse(netrc);
 };
 
@@ -29,34 +31,33 @@ module.exports = exports = function(file) {
  * @return {Object}
  * @api public
  */
+
 exports.parse = function(content) {
   // Remove comments
   var lines = content.split('\n');
   for (var n in lines) {
     var i = lines[n].indexOf('#');
-    if (i > -1) lines[n] = lines[n].substring(0,i);
+    if (i > -1) lines[n] = lines[n].substring(0, i);
   }
   content = lines.join('\n');
 
-  var tokens = content.split(/[ \t\n\r]+/)
-    , machines = {}
-    , m = null
-    , key = null;
+  var tokens = content.split(/[ \t\n\r]+/);
+  var machines = {};
+  var m = null;
+  var key = null;
 
-  //if first index in array is empty string, strip it off (happens when first line of file is comment. Breaks the parsing)
-  if (tokens[0] === "") {
-    tokens.shift();
-  }
+  // if first index in array is empty string, strip it off (happens when first line of file is comment. Breaks the parsing)
+  if (tokens[0] === '') tokens.shift();
 
-  for(var i = 0; i < tokens.length; i+=2) {
-    var key = tokens[i]
-      , value = tokens[i+1];
+  for(var i = 0, key, value; i < tokens.length; i+=2) {
+    key = tokens[i];
+    value = tokens[i+1];
 
     // Whitespace
-    if(!key || !value) continue;
+    if (!key || !value) continue;
 
     // We have a new machine definition
-    if(key === "machine") {
+    if (key === 'machine') {
       m = {};
       machines[value] = m;
     }
@@ -74,20 +75,20 @@ exports.parse = function(content) {
  * @param {Object} machines as returned by `netrc.parse`
  * @return {String} text of the netrc file
  */
+
 exports.format = function format(machines){
-    var lines = [],
-        keys = Object.getOwnPropertyNames(machines).sort();
-    keys.forEach(function(key){
-        lines.push('machine ' + key);
-        var machine = machines[key];
-        var attrs = Object.getOwnPropertyNames(machine).sort();
-        attrs.forEach(function(attr){
-            if(typeof(machine[attr]) === 'string'){
-                lines.push('    ' + attr + ' ' + machine[attr]);
-            }
-        });
+  var lines = [];
+  var keys = Object.getOwnPropertyNames(machines).sort();
+
+  keys.forEach(function(key){
+    lines.push('machine ' + key);
+    var machine = machines[key];
+    var attrs = Object.getOwnPropertyNames(machine).sort();
+    attrs.forEach(function(attr){
+      if (typeof(machine[attr]) === 'string') lines.push('    ' + attr + ' ' + machine[attr]);
     });
-    return lines.join('\n');
+  });
+  return lines.join('\n');
 };
 
 /**
@@ -96,10 +97,11 @@ exports.format = function format(machines){
  * @param {Object} machines as returned by `netrc.parse`
  * @api public
  */
+
 exports.save = function save(machines){
   var home = getHomePath();
-  var destFile = join(home, ".netrc");
-  var data = exports.format(machines) + "\n";
+  var destFile = join(home, '.netrc');
+  var data = exports.format(machines) + '\n';
   fs.writeFileSync(destFile, data);
 };
 
@@ -109,6 +111,7 @@ exports.save = function save(machines){
  * @return {String} path to home directory
  * @api private
  */
+
 function getHomePath() {
   return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 }
